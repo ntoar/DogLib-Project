a24fe1f0fd8b2ff6b2c389fbaf151b96ea3bd9dd
ntoar <noeltomas@gmail.com>
2019-10-29 10:52:25 +0100

    Change the file path of the dogstest.xml in the DogLibTest test methods

--- maven-doglib/src/test/java/com/doglib/DogLibTest.java
+++ maven-doglib/src/test/java/com/doglib/DogLibTest.java
@@ -29,14 +29,14 @@ public class DogLibTest {
 	
 	@Test
 	public void averageWeightTest() {
-		DogLib dl = new DogLib("C:\\Users\\USUARIO\\assignment\\dogstest.xml");
+		DogLib dl = new DogLib("src\\test\\resources\\dogstest.xml");
 		
 		assertEquals(true, dl.averageWeight("Shiba Inu")==17);
 	}
 	
 	@Test
 	public void averageWeightPerBreedTest() {
-		DogLib dl = new DogLib("C:\\Users\\USUARIO\\assignment\\dogstest.xml");
+		DogLib dl = new DogLib("src\\test\\resources\\dogstest.xml");
 		Map<String,Double> breedStats = dl.averageWeightPerBreed();
 		
 		assertEquals(true, breedStats.get("Shiba Inu")==17 && breedStats.get("Siberian Husky")==25);
@@ -44,7 +44,7 @@ public class DogLibTest {
 	
 	@Test
 	public void dogsByConditionTest() {
-		DogLib dl = new DogLib("C:\\Users\\USUARIO\\assignment\\dogstest.xml");
+		DogLib dl = new DogLib("src\\test\\resources\\dogstest.xml");
 		List<Dog> listdog = dl.dogsByCondition("breed", "eq", "Shiba Inu");
 		
 		assertEquals(true, listdog.size()==1);
@@ -52,7 +52,7 @@ public class DogLibTest {
 	
 	@Test
 	public void oldestDogAfterDateTest() {
-		DogLib dl = new DogLib("C:\\Users\\USUARIO\\assignment\\dogstest.xml");
+		DogLib dl = new DogLib("src\\test\\resources\\dogstest.xml");
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 		LocalDate date=LocalDate.parse("10-01-2012",formatter);		
 		Dog d = dl.oldestDogAfterDate(date);
